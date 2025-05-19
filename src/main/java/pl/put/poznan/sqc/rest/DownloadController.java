3527916f02e04579f80cb799c6ff69e76cd16d4e
kacperkuznik <kuznikkacper@gmail.com>
2025-05-20 00:54:58 +0200

    feat: rest_api boilerplate

---
+++ src/main/java/pl/put/poznan/sqc/rest/DownloadController.java
@@ -0,0 +1,23 @@
+package pl.put.poznan.sqc.rest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/download")
+public class DownloadController {
+
+    private static final Logger logger = LoggerFactory.getLogger(CountStepsController.class);
+
+    @RequestMapping(method = RequestMethod.POST, produces = "application/json")
+    public String post(@RequestBody Map<String, List<String>> body) {
+        List<String> steps = body.get("steps");
+        // count number of steps
+        return steps.toString();
+    }
+
+}
