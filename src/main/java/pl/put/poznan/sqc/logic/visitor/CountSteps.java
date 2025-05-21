524a212ee31ddd17df77480ee124c08be291dc9b
Jakub <jakub.radziejewski04@gmail.com>
2025-05-21 19:35:17 +0200

    feat: CountStepsController functionality

--- src/main/java/pl/put/poznan/sqc/logic/visitor/CountSteps.java
+++ src/main/java/pl/put/poznan/sqc/logic/visitor/CountSteps.java
@@ -1,5 +1,31 @@
+// src/main/java/pl/put/poznan/sqc/logic/visitor/CountSteps.java
 package pl.put.poznan.sqc.logic.visitor;
 
-public class CountSteps {
+import pl.put.poznan.sqc.logic.model.Scenario;
+import pl.put.poznan.sqc.logic.model.Step;
 
+public class CountSteps implements Visitor {
+    private int totalSteps = 0;
+
+    @Override
+    public void visit(Scenario scenario) {
+        for (Step step : scenario.getSteps()) {
+            step.accept(this);
+        }
+    }
+
+    @Override
+    public void visit(Step step) {
+        totalSteps++; // Count this step
+        if (step.getSubsteps() != null) {
+            for (Step substep : step.getSubsteps()) {
+                substep.accept(this);
+            }
+        }
+    }
+
+    @Override
+    public Object getResult() {
+        return totalSteps;
+    }
 }
