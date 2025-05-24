abc46213dd5c72529e135e05d45a4d0f74512215
kacperkuznik <kuznikkacper@gmail.com>
2025-05-24 16:23:35 +0200

    feat: downloading file as txt

--- src/main/java/pl/put/poznan/sqc/rest/DownloadController.java
+++ src/main/java/pl/put/poznan/sqc/rest/DownloadController.java
@@ -2,10 +2,14 @@ package pl.put.poznan.sqc.rest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import pl.put.poznan.sqc.logic.model.Scenario;
 import pl.put.poznan.sqc.logic.visitor.Download;
 
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 @RestController
@@ -13,8 +17,8 @@ import java.util.List;
 public class DownloadController {
     private static final Logger logger = LoggerFactory.getLogger(DownloadController.class);
 
-    @RequestMapping(method = RequestMethod.POST, produces = "text/plain")
-    public String post(@RequestBody Scenario scenario) {
+    @PostMapping(produces = MediaType.TEXT_PLAIN_VALUE)
+    public ResponseEntity<byte[]> post(@RequestBody Scenario scenario) {
         logger.info("Preparing scenario for download: {}", scenario.getTitle());
 
         Download visitor = new Download();
@@ -24,8 +28,18 @@ public class DownloadController {
         List<String> numberedSteps = (List<String>) visitor.getResult();
 
         String text = String.join("\n", numberedSteps);
-        logger.debug("returned text: \n{}", text);
-        logger.info("Downloading file");
-        return text;
+        logger.debug("Returned text: \n{}", text);
+
+        byte[] content = text.getBytes(StandardCharsets.UTF_8);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.TEXT_PLAIN);
+        headers.setContentDispositionFormData("attachment", "scenario.txt");
+        headers.setContentLength(content.length);
+        logger.info("Downloading text");
+
+        return ResponseEntity.ok()
+                .headers(headers)
+                .body(content);
     }
 }
